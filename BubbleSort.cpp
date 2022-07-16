d874a8ffc1a73cf3f4d90d87d57bbc35112655f9
khyatig95 <85378102+khyatig95@users.noreply.github.com>
2022-07-16 12:41:31 -0700

    Update BubbleSort.cpp

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -1,148 +1,50 @@
-{\rtf1\ansi\ansicpg1252\cocoartf1348\cocoasubrtf170
-{\fonttbl\f0\fnil\fcharset0 Menlo-Regular;\f1\fnil\fcharset0 Menlo-Bold;}
-{\colortbl;\red255\green255\blue255;\red63\green127\blue95;\red127\green0\blue85;\red42\green0\blue255;
-\red100\green70\blue50;}
-\margl1440\margr1440\vieww10800\viewh8400\viewkind0
-\deftab720
-\pard\pardeftab720
+//Time Complexity : O(N^2)
+//Space Complexity : O(1)
 
-\f0\fs24 \cf2 //Time Complexity : O(N^2)\cf0 \
-\cf2 //Space Complexity : O(1)\cf0 \
-\
-\pard\pardeftab720
+#include <iostream>
+using namespace std;
 
-\f1\b \cf3 #include
-\f0\b0 \cf0  \cf4 <iostream>\cf0 \
+template <typename T>
+void swap(T* x, T* y) {
+	cout << "Swapping " << *x << " and " << *y << endl;
+	T temp = *x;
+	*x = *y;
+	*y = temp;
+}
 
-\f1\b \cf3 using
-\f0\b0 \cf0  
-\f1\b \cf3 namespace
-\f0\b0 \cf0  std;\
-\
+template <typename T>
+void printArr(T* arr, int size) {
+	cout << "[ " ;
+	for (int i=0; i<size; i++)
+		cout << arr[i] << " ";
+	cout << "]" << endl;
+}
 
-\f1\b \cf3 template
-\f0\b0 \cf0  <
-\f1\b \cf3 typename
-\f0\b0 \cf0  
-\f1\b \cf5 T
-\f0\b0 \cf0 >\
+template <typename T>
+T* BubbleSort(T* arr, int size) {
+	if (size == 0 || size == 1) {
+		cout << "Array is empty or has just one element" << endl;
+		return arr;
+	}
 
-\f1\b \cf3 void
-\f0\b0 \cf0  
-\f1\b swap
-\f0\b0 (
-\f1\b \cf5 T
-\f0\b0 \cf0 * x, 
-\f1\b \cf5 T
-\f0\b0 \cf0 * y) \{\
-	cout << \cf4 "Swapping "\cf0  << *x << \cf4 " and "\cf0  << *y << endl;\
-	
-\f1\b \cf5 T
-\f0\b0 \cf0  temp = *x;\
-	*x = *y;\
-	*y = temp;\
-\}\
-\
+	for (int i=0; i<size-1; i++) {
+		cout << "Comparing " << arr[i] << " with " << arr[i+1] << endl;
+		if (arr[i] > arr[i+1])
+			swap(&arr[i], &arr[i+1]);
+	}
+	cout << endl;
+	//Recursion with size-1 as at the end of each pass, we move the largest element at the correct position
+	arr = BubbleSort(arr, size-1);
+	return arr;
+}
 
-\f1\b \cf3 template
-\f0\b0 \cf0  <
-\f1\b \cf3 typename
-\f0\b0 \cf0  
-\f1\b \cf5 T
-\f0\b0 \cf0 >\
-
-\f1\b \cf3 void
-\f0\b0 \cf0  
-\f1\b printArr
-\f0\b0 (
-\f1\b \cf5 T
-\f0\b0 \cf0 * arr, 
-\f1\b \cf3 int
-\f0\b0 \cf0  size) \{\
-	cout << \cf4 "[ "\cf0  ;\
-	
-\f1\b \cf3 for
-\f0\b0 \cf0  (
-\f1\b \cf3 int
-\f0\b0 \cf0  i=0; i<size; i++)\
-		cout << arr[i] << \cf4 " "\cf0 ;\
-	cout << \cf4 "]"\cf0  << endl;\
-\}\
-\
-
-\f1\b \cf3 template
-\f0\b0 \cf0  <
-\f1\b \cf3 typename
-\f0\b0 \cf0  
-\f1\b \cf5 T
-\f0\b0 \cf0 >\
-\pard\pardeftab720
-
-\f1\b \cf5 T
-\f0\b0 \cf0 * 
-\f1\b BubbleSort
-\f0\b0 (
-\f1\b \cf5 T
-\f0\b0 \cf0 * arr, 
-\f1\b \cf3 int
-\f0\b0 \cf0  size) \{\
-	
-\f1\b \cf3 if
-\f0\b0 \cf0  (size == 0 || size == 1) \{\
-		cout << \cf4 "Array is empty or has just one element"\cf0  << endl;\
-		
-\f1\b \cf3 return
-\f0\b0 \cf0  arr;\
-	\}\
-\
-	
-\f1\b \cf3 for
-\f0\b0 \cf0  (
-\f1\b \cf3 int
-\f0\b0 \cf0  i=0; i<size-1; i++) \{\
-		cout << \cf4 "Comparing "\cf0  << arr[i] << \cf4 " with "\cf0  << arr[i+1] << endl;\
-		
-\f1\b \cf3 if
-\f0\b0 \cf0  (arr[i] > arr[i+1])\
-			swap(&arr[i], &arr[i+1]);\
-	\}\
-	cout << endl;\
-	\cf2 //Recursion with size-1 as at the end of each pass, we move the largest element at the correct position\cf0 \
-	arr = BubbleSort(arr, size-1);\
-	
-\f1\b \cf3 return
-\f0\b0 \cf0  arr;\
-\}\
-\
-\pard\pardeftab720
-
-\f1\b \cf3 int
-\f0\b0 \cf0  
-\f1\b main
-\f0\b0 () \{\
-	
-\f1\b \cf3 char
-\f0\b0 \cf0  arr[] = \{\cf4 'a'\cf0 , \cf4 'x'\cf0 , \cf4 'k'\cf0 , \cf4 'h'\cf0 , \cf4 's'\cf0 \};\
-	
-\f1\b \cf3 int
-\f0\b0 \cf0  size = 
-\f1\b \cf3 sizeof
-\f0\b0 \cf0 (arr)/
-\f1\b \cf3 sizeof
-\f0\b0 \cf0 (arr[0]); \cf2 //Will give number of elements in array\cf0 \
-	
-\f1\b \cf3 char
-\f0\b0 \cf0 * res;\
-	cout << \cf4 "Unsorted array: "\cf0  << endl;\
-	printArr<
-\f1\b \cf3 char
-\f0\b0 \cf0 >(arr, size);\
-	res = BubbleSort<
-\f1\b \cf3 char
-\f0\b0 \cf0 >(arr, size);\
-	cout << \cf4 "Sorted array: "\cf0  << endl;\
-	printArr<
-\f1\b \cf3 char
-\f0\b0 \cf0 >(res, size);\
-\}\
+int main() {
+	char arr[] = {'a', 'x', 'k', 'h', 's'};
+	int size = sizeof(arr)/sizeof(arr[0]); //Will give number of elements in array
+	char* res;
+	cout << "Unsorted array: " << endl;
+	printArr<char>(arr, size);
+	res = BubbleSort<char>(arr, size);
+	cout << "Sorted array: " << endl;
+	printArr<char>(res, size);
 }
